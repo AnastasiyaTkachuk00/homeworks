3b49e108c5376630cd2e211a03ff4bfe9837f7f1
AnastasiyaTkachuk00 <tkachuk_nastasya@mail.ru>
2022-11-27 16:16:31 +0300

    Update task_three.py

--- task_three.py
+++ task_three.py
@@ -1,9 +1,10 @@
 import random
 
 mylist = []
-for i in (range(0, 10)):
+for i in range(10):
     mylist.append(random.randint(-100, 101))
 print(mylist)
 
 mylist[2] = random.choice(mylist)
+del mylist[6]
 print(mylist)
